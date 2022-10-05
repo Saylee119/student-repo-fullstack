ed8e5e91eb8633b56de33c85f65ec4bbf49b8fbc
SayleeMJ <jagtapsaylee.2020@gmail.com>
2022-10-04 18:59:58 -0700

    Modified FizzBuzz.js

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -8,9 +8,21 @@ Write a program that writes all the numbers from 1 to 100, with some exceptions:
 Use console.log() to write the proper output to the command line.
 
 **/
-
 const fizzbuzz = () => {
-  // Add your code here
+  for (var i = 1; i < 101; i++) {
+    if (i % 3 == 0 && i % 5 == 0){
+      console.log("FizzBuzz");
+    } 
+    else if (i % 3 == 0){ 
+      console.log("Fizz");
+    }
+    else if (i % 5 == 0){
+      console.log("Buzz");
+    } 
+    else{ 
+      console.log(i);
+    }
+}
 };
 
 fizzbuzz();
