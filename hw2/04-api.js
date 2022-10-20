323bc520d2f7f180343a104341dda5b603a3ee7c
Saylee Jagtap <sjagtap@pdx.edu>
2022-10-19 18:58:31 -0700

    Updated homework2

--- hw2/04-api.js
+++ hw2/04-api.js
@@ -1,7 +1,29 @@
 /** Exercise 04 - API **/
 
-const url = 'https://restcountries.com/v3.1/all';
+const url = "https://restcountries.com/v2/all";
 
-// Add your code here
+const getData = () => {
 
-getData(url);
+    const cList = document.getElementById("results"); //get the item id from html
+    fetch(url)
+      .then((data) => data.json())
+      .then((data) => {
+        cList.innerHTML = "";
+        const countryList = data;
+
+        countryList.forEach((item) => {
+        
+          const element = document.createElement("li");
+          const text = `${item.name} - ${parseInt(item.population).toLocaleString()}`;
+          element.innerText = text;
+          cList.appendChild(element);
+
+        });
+      })
+      .catch((e) => { // checking for errors
+        console.log("error", e);
+        cList.innerHTML = "There in an error";
+      });
+  };
+
+  getData(url);
