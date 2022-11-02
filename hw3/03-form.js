95a5b0fa997c6007f4b1bdcfc27d06ff212aedd3
Saylee Jagtap <sjagtap@pdx.edu>
2022-11-02 14:49:05 -0700

    Commiting homework3

--- hw3/03-form.js
+++ hw3/03-form.js
@@ -1,10 +1,44 @@
-const http = require('http');
-const port = process.env.PORT || 5001;
+const http = require("http");
+const port = process.env.PORT || 5000;
+const staticServe = require("node-static");
+const querystring = require("querystring");
 
-// http://localhost:5001/form should return a form with input elements for username, email, and submit button
+const file = new staticServe.Server("./");
 
-// http://localhost:5001/submit should return all the data the user entered
-
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    file.serveFile("03-form.html", 200, {}, req, res);
+  } else if (req.method === "POST" && req.url === "/submit") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      const info = querystring.parse(body);
+      res.setHeader("Content-Type", "text/html");
+      res.write(`Name: ${info.name}<br>`);
+      res.write(`Email: ${info.email}<br>`);
+      res.write(
+        `Comments: ${
+          info.feedback ? info.feedback : "N/A"
+        }<br>`
+      );
+      res.write(
+        `Newsletter: ${
+          info.newsletter
+            ? "Yes, I would like to receive newsletters"
+            : "No, thank you."
+        }<br>`
+      );
+      res.end();
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(404);
+    res.write(`<h1>Please go back and fill the information.</h1>`);
+    res.end();
+  }
+});
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
